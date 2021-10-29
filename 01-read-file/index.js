02871bba2e1bca5602407f6837789ccea742f90e
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2021-10-30 01:50:14 +0300

    refactor: delete console.log

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path/posix');
+const { stdout } = require('process');
 
 const stream = fs.createReadStream(
   path.join(__dirname, '/text.txt'),
   'utf8'
 );
-stream.on('data', (data) => console.log(data));
-stream.on('error', (err) => console.log(`Err: ${err}`));
+
+stream.on('data', (data) => stdout.write(data));
+stream.on('error', (err) => stdout.write(`Err: ${err}`));
